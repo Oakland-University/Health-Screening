8da3764b4e9069a655b2851d8bbfa831802157b3
Aaron Goodfellow <amgoodfellow@oakland.edu>
2020-06-12 13:06:33 -0400

    Adds schema before sql statement #4

--- src/main/java/edu/oakland/healthscreening/dao/Constants.java
+++ src/main/java/edu/oakland/healthscreening/dao/Constants.java
@@ -3,7 +3,7 @@ package edu.oakland.healthscreening.dao;
 public class Constants {
 
   public static final String INSERT_HEALTH_INFO =
-      (" INSERT INTO health_screening (account_type, pidm, email, phone, is_coughing, is_feverish, is_exposed)  "
+      (" INSERT INTO screening.health_screening (account_type, pidm, email, phone, is_coughing, is_feverish, is_exposed)  "
           + " VALUES                                                                                             "
           + " (?, ?, ?, ?, ?, ?, ?)                                                                              "
           + "                                                                                                    ");
