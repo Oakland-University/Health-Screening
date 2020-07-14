a813146e2c839178e3a553889dd046ecfe995370
Aaron Goodfellow <amgoodfellow@oakland.edu>
2020-07-14 07:48:51 -0400

    Removes console.log #8

--- src/main/react/src/actions/main-actions.js
+++ src/main/react/src/actions/main-actions.js
@@ -85,7 +85,6 @@ export const press_modal_button = () => (dispatch, getState) => {
     } = getState()
 
     if (cough !== null && fever !== null && exposure !== null) {
-      console.log(fever, cough, exposure, email)
       submit_form(
         { name, email, phone, account_type },
         { fever, cough, exposure }
