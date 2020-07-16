b2e68c4e6cc94f9f7f5184725779b0ae9268eeef
Josh Wargo <wargo@oakland.edu>
2020-07-16 09:29:28 -0400

    Removes semi colon.

--- src/main/react/src/App.js
+++ src/main/react/src/App.js
@@ -7,7 +7,7 @@ import CardContent from '@material-ui/core/CardContent'
 import CardActions from '@material-ui/core/CardActions'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
-import ComingToCampus from './components/ComingToCampus';
+import ComingToCampus from './components/ComingToCampus'
 import HealthQuestions from './components/HealthQuestions'
 import UserInfo from './components/UserInfo'
 import FinalPage from './components/FinalPage'
