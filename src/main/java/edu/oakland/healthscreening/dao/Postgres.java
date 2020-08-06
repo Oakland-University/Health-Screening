713d0cd2ef19f90d170ef704f95ccf905316d754
Josh Wargo <wargo@oakland.edu>
2020-08-06 08:04:11 -0400

    Adds second datasource of Banner, and configures both datasources. #28

--- src/main/java/edu/oakland/healthscreening/dao/Postgres.java
+++ src/main/java/edu/oakland/healthscreening/dao/Postgres.java
@@ -8,10 +8,12 @@ import edu.oakland.healthscreening.model.Pledge;
 
 import java.util.List;
 import java.util.Optional;
+import javax.sql.DataSource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -19,11 +21,18 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class Postgres {
 
-  @Autowired JdbcTemplate postgresTemplate;
+  @Autowired JdbcTemplate postgresJdbcTemplate;
+
   private final Logger log = LoggerFactory.getLogger("health-screening");
 
+  @Autowired
+  @Qualifier("postgresDataSource")
+  public void Postgres(DataSource dataSource) {
+    this.postgresJdbcTemplate = new JdbcTemplate(dataSource);
+  }
+
   public void saveHealthInfo(HealthInfo info) {
-    postgresTemplate.update(
+    postgresJdbcTemplate.update(
         INSERT_HEALTH_INFO,
         info.getAccountType().toString(),
         info.getPidm(),
@@ -36,7 +45,7 @@ public class Postgres {
   }
 
   public void saveAnalyticInfo(HealthInfo info) {
-    postgresTemplate.update(
+    postgresJdbcTemplate.update(
         INSERT_ANALYTICS,
         info.getAccountType().toString(),
         info.isCoughing(),
@@ -45,7 +54,7 @@ public class Postgres {
   }
 
   public void savePledge(Pledge pledge) {
-    postgresTemplate.update(
+    postgresJdbcTemplate.update(
         INSERT_PLEDGE,
         pledge.getEmail(),
         pledge.isFaceCovering(),
@@ -54,20 +63,20 @@ public class Postgres {
   }
 
   public AnalyticInfo getAnalyticInfo(String interval) {
-    return postgresTemplate.queryForObject(GET_ANALYTIC_INFO, AnalyticInfo.mapper, interval);
+    return postgresJdbcTemplate.queryForObject(GET_ANALYTIC_INFO, AnalyticInfo.mapper, interval);
   }
 
   public List<HealthInfo> getHealthInfo() {
-    return postgresTemplate.query(GET_ALL_RESPONSES, HealthInfo.mapper);
+    return postgresJdbcTemplate.query(GET_ALL_RESPONSES, HealthInfo.mapper);
   }
 
   public Optional<HealthInfo> getRecentSubmission(String pidm, String email) {
     try {
-      Pledge pledge = postgresTemplate.queryForObject(GET_RECENT_PLEDGE, Pledge.mapper, email);
+      Pledge pledge = postgresJdbcTemplate.queryForObject(GET_RECENT_PLEDGE, Pledge.mapper, email);
       HealthInfo info = new HealthInfo();
 
       if (pledge.fullAgreement()) {
-        info = postgresTemplate.queryForObject(GET_RECENT_INFO, HealthInfo.mapper, pidm);
+        info = postgresJdbcTemplate.queryForObject(GET_RECENT_INFO, HealthInfo.mapper, pidm);
       }
 
       info.setPledge(pledge);
@@ -80,7 +89,8 @@ public class Postgres {
 
   public Optional<HealthInfo> getRecentSubmission(String pidm) {
     try {
-      return Optional.of(postgresTemplate.queryForObject(GET_RECENT_INFO, HealthInfo.mapper, pidm));
+      return Optional.of(
+          postgresJdbcTemplate.queryForObject(GET_RECENT_INFO, HealthInfo.mapper, pidm));
     } catch (EmptyResultDataAccessException e) {
       return Optional.empty();
     }
@@ -89,13 +99,14 @@ public class Postgres {
   public Optional<HealthInfo> getGuestSubmission(String name, String email, String phone) {
     try {
       return Optional.of(
-          postgresTemplate.queryForObject(GET_GUEST_INFO, HealthInfo.mapper, name, email, phone));
+          postgresJdbcTemplate.queryForObject(
+              GET_GUEST_INFO, HealthInfo.mapper, name, email, phone));
     } catch (EmptyResultDataAccessException e) {
       return Optional.empty();
     }
   }
 
   public void deleteOldRecords() {
-    postgresTemplate.update(DELETE_OLD_RECORDS);
+    postgresJdbcTemplate.update(DELETE_OLD_RECORDS);
   }
 }
