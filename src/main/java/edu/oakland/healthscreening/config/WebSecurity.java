c94ced4b8acf4a67cbabea70409d72fe0e794b46
Aaron Goodfellow <amgoodfellow@oakland.edu>
2020-09-10 14:49:58 -0400

    Adds default prop value #50

--- src/main/java/edu/oakland/healthscreening/config/WebSecurity.java
+++ src/main/java/edu/oakland/healthscreening/config/WebSecurity.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.savedrequest.NullRequestCache;
 
-@ConditionalOnExpression("${ldap.enabled}")
+@ConditionalOnExpression("${ldap.enabled:true}")
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
