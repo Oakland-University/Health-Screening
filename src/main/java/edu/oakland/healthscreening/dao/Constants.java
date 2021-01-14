598a742c3bddcfcd2f6a7d664fe59ed330ec0645
Aaron Goodfellow <amgoodfellow@oakland.edu>
2021-01-14 11:25:26 -0500

    Adds account type to analytic info query #70

--- src/main/java/edu/oakland/healthscreening/dao/Constants.java
+++ src/main/java/edu/oakland/healthscreening/dao/Constants.java
@@ -11,6 +11,7 @@ public class Constants {
 
   public static final String GET_ANONYMOUS_ANALYTIC_INFO =
       (" SELECT                                                                           "
+              + "     account_type,                                                       "
               + "     COUNT(*) AS total,                                                  "
               + "     SUM                                                                 "
               + "     (CASE                                                               "
