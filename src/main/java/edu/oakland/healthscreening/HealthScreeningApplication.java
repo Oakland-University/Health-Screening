9299af7914879c88f5c2cae70f47e4fd3fc60808
Aaron Goodfellow <amgoodfellow@oakland.edu>
2021-07-22 16:08:23 -0400

    Adds FreeMarker dependency and config bean #93
    FreeMarker is a templating engine similar to Thymeleaf, but with a
    simpler syntax

--- src/main/java/edu/oakland/healthscreening/HealthScreeningApplication.java
+++ src/main/java/edu/oakland/healthscreening/HealthScreeningApplication.java
@@ -1,16 +1,34 @@
 package edu.oakland.healthscreening;
 
+import freemarker.cache.ClassTemplateLoader;
+import freemarker.cache.TemplateLoader;
+import freemarker.template.Configuration;
 import org.apereo.portal.soffit.renderer.SoffitApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 
 @ComponentScan({"edu.oakland.soffit.auth", "edu.oakland.healthscreening"})
 @EnableScheduling
 @SoffitApplication
 @SpringBootApplication
 public class HealthScreeningApplication {
+
+  @Bean
+  public FreeMarkerConfigurer freemarkerClassLoaderConfig() {
+    Configuration configuration = new Configuration(Configuration.VERSION_2_3_30);
+    // Print boolean values as the computer sees them (i.e. 'true' and 'false')
+    configuration.setBooleanFormat("c");
+    // Look for 'ftl' files in src/main/resources/templates - and keep them in the classpath
+    TemplateLoader templateLoader = new ClassTemplateLoader(this.getClass(), "/templates");
+    configuration.setTemplateLoader(templateLoader);
+    FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer();
+    freeMarkerConfigurer.setConfiguration(configuration);
+    return freeMarkerConfigurer;
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(HealthScreeningApplication.class, args);
