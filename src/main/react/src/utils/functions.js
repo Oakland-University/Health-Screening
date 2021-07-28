2852fd45c21ba1ab97e714831ea879ca10941607
Aaron Goodfellow <amgoodfellow@oakland.edu>
2021-07-28 10:46:17 -0400

    Takes suggestion from @nhwoodle to reduce line count #98

--- src/main/react/src/utils/functions.js
+++ src/main/react/src/utils/functions.js
@@ -1,7 +1,5 @@
 export const allowed_on_campus = (data) => {
-  const { symptomatic, exposed, fully_vaccinated } = data
-
-  return !(symptomatic || (exposed && !fully_vaccinated))
+  return !(data.symptomatic || (data.exposed && !data.fully_vaccinated))
 }
 
 export const all_questions_non_null = (symptom_data) => {
