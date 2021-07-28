f2696fbbf5974820926165fdfe7b847a7762d347
Aaron Goodfellow <amgoodfellow@oakland.edu>
2021-07-28 13:28:43 -0400

    Removes specific symptom questions from backend model #100

--- src/main/java/edu/oakland/healthscreening/dao/Postgres.java
+++ src/main/java/edu/oakland/healthscreening/dao/Postgres.java
@@ -45,20 +45,9 @@ public class Postgres {
                 new SqlParameter("p_email", Types.VARCHAR),
                 new SqlParameter("p_phone", Types.VARCHAR),
                 new SqlParameter("p_name", Types.VARCHAR),
-                new SqlParameter("p_is_coughing", Types.BOOLEAN),
-                new SqlParameter("p_is_feverish", Types.BOOLEAN),
                 new SqlParameter("p_is_exposed", Types.BOOLEAN),
                 new SqlParameter("p_supervisor_email", Types.VARCHAR),
-                new SqlParameter("p_is_short_of_breath", Types.BOOLEAN),
-                new SqlParameter("p_has_sore_throat", Types.BOOLEAN),
-                new SqlParameter("p_is_congested", Types.BOOLEAN),
-                new SqlParameter("p_has_muscle_aches", Types.BOOLEAN),
-                new SqlParameter("p_has_lost_taste_smell", Types.BOOLEAN),
-                new SqlParameter("p_has_headache", Types.BOOLEAN),
-                new SqlParameter("p_has_diarrhea", Types.BOOLEAN),
-                new SqlParameter("p_is_nauseous", Types.BOOLEAN),
-                new SqlParameter("p_is_fully_vaccinated", Types.BOOLEAN),
-                new SqlParameter("p_has_tested_positive", Types.BOOLEAN));
+                new SqlParameter("p_is_fully_vaccinated", Types.BOOLEAN));
 
     final SqlParameterSource parameterSource =
         new MapSqlParameterSource()
@@ -67,20 +56,9 @@ public class Postgres {
             .addValue("p_email", info.getEmail())
             .addValue("p_phone", info.getPhone())
             .addValue("p_name", info.getName())
-            .addValue("p_is_coughing", info.isCoughing())
-            .addValue("p_is_feverish", info.isFeverish())
             .addValue("p_is_exposed", info.isExposed())
             .addValue("p_supervisor_email", info.getSupervisorEmail())
-            .addValue("p_is_short_of_breath", info.isShortOfBreath())
-            .addValue("p_has_sore_throat", info.isSoreThroat())
-            .addValue("p_is_congested", info.isCongested())
-            .addValue("p_has_muscle_aches", info.isMuscleAche())
-            .addValue("p_has_lost_taste_smell", info.isLossOfTasteOrSmell())
-            .addValue("p_has_headache", info.isHeadache())
-            .addValue("p_has_diarrhea", info.isDiarrhea())
-            .addValue("p_is_nauseous", info.isNauseous())
-            .addValue("p_is_fully_vaccinated", info.getFullyVaccinated())
-            .addValue("p_has_tested_positive", info.isTestedPositive());
+            .addValue("p_is_fully_vaccinated", info.getFullyVaccinated());
 
     log.debug("Preparing to save health info: {}", info);
 
--- src/main/java/edu/oakland/healthscreening/model/HealthInfo.java
+++ src/main/java/edu/oakland/healthscreening/model/HealthInfo.java
@@ -25,35 +25,15 @@ public class HealthInfo {
   @Email(message = "Email field should be a valid address")
   private String email;
 
-  private boolean congested;
-  private boolean coughing;
-  private boolean diarrhea;
+  private boolean symptomatic;
   private boolean exposed;
-  private boolean testedPositive;
-  private boolean feverish;
-  private boolean headache;
-  private boolean lossOfTasteOrSmell;
-  private boolean muscleAche;
-  private boolean nauseous;
-  private boolean shortOfBreath;
-  private boolean soreThroat;
+  private boolean allowed_on_campus;
   private Boolean fullyVaccinated;
   private Timestamp submissionTime;
   private String supervisorEmail;
 
   public boolean shouldStayHome() {
-    return (coughing
-        || feverish
-        || (exposed && !Boolean.TRUE.equals(fullyVaccinated))
-        || shortOfBreath
-        || soreThroat
-        || congested
-        || muscleAche
-        || lossOfTasteOrSmell
-        || headache
-        || diarrhea
-        || nauseous
-        || testedPositive);
+    return (symptomatic || (exposed && !Boolean.TRUE.equals(fullyVaccinated)));
   }
 
   public String summarize() {
@@ -115,19 +95,8 @@ public class HealthInfo {
         info.setName(rs.getString("name"));
         info.setEmail(rs.getString("email"));
         info.setPhone(rs.getString("phone"));
-        info.setCoughing(rs.getBoolean("is_coughing"));
-        info.setFeverish(rs.getBoolean("is_feverish"));
         info.setExposed(rs.getBoolean("is_exposed"));
-        info.setShortOfBreath(rs.getBoolean("is_short_of_breath"));
-        info.setSoreThroat(rs.getBoolean("has_sore_throat"));
-        info.setCongested(rs.getBoolean("is_congested"));
-        info.setMuscleAche(rs.getBoolean("has_muscle_aches"));
-        info.setLossOfTasteOrSmell(rs.getBoolean("has_lost_taste_smell"));
-        info.setHeadache(rs.getBoolean("has_headache"));
-        info.setDiarrhea(rs.getBoolean("has_diarrhea"));
-        info.setNauseous(rs.getBoolean("is_nauseous"));
         info.setFullyVaccinated(rs.getObject("is_fully_vaccinated", Boolean.class));
-        info.setTestedPositive(rs.getBoolean(("has_tested_positive")));
         info.setSubmissionTime(rs.getTimestamp("submission_time"));
 
         return info;
@@ -135,60 +104,6 @@ public class HealthInfo {
 
   private String responseSummary() {
     final List<String> summaryList = new LinkedList<>();
-
-    if (coughing) {
-      summaryList.add("is experiencing a cough");
-    }
-
-    if (feverish) {
-      summaryList.add("is experiencing a fever");
-    }
-
-    if (exposed) {
-      if (fullyVaccinated == null) {
-        summaryList.add("has been exposed to someone with COVID");
-      } else if (Boolean.TRUE.equals(fullyVaccinated)) {
-        summaryList.add("has been exposed to someone with COVID, but has been vaccinated");
-      } else {
-        summaryList.add("has been exposed to someone with COVID, and has not been vaccinated");
-      }
-    }
-
-    if (shortOfBreath) {
-      summaryList.add("is experiencing shortness of breath");
-    }
-
-    if (soreThroat) {
-      summaryList.add("is experiencing a sore throat");
-    }
-
-    if (congested) {
-      summaryList.add("is experiencing new, unexplained congestion");
-    }
-
-    if (muscleAche) {
-      summaryList.add("is experiencing muscle aches");
-    }
-
-    if (lossOfTasteOrSmell) {
-      summaryList.add("is experiencing a loss of taste or smell");
-    }
-
-    if (headache) {
-      summaryList.add("is experiencing a headache");
-    }
-
-    if (diarrhea) {
-      summaryList.add("is experiencing diarrhea");
-    }
-
-    if (nauseous) {
-      summaryList.add("is experiencing nausea or vomiting");
-    }
-
-    if (testedPositive) {
-      summaryList.add("has tested positive for COVID-19 within the last 10 days");
-    }
 
     return String.join("\n\t- ", summaryList);
   }
