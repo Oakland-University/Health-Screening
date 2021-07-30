62c313e09f2eb1c1dee3e341b8ea4ab4a255bdb3
Aaron Goodfellow <amgoodfellow@oakland.edu>
2021-07-30 01:12:21 -0400

    Removes CSV string method #100

--- src/main/java/edu/oakland/healthscreening/model/AnalyticInfo.java
+++ src/main/java/edu/oakland/healthscreening/model/AnalyticInfo.java
@@ -29,49 +29,6 @@ public class AnalyticInfo {
   private AccountType accountType;
   private Map<AccountType, AnalyticInfo> subTypeAnalytics;
 
-  public String toCSVString() {
-    final String dateString = LocalDateTime.now().format(DateTimeFormatter.ofPattern("MM/dd/yyyy"));
-    return
-    /*id*/ ","
-        + dateString
-        + ","
-        + "MI,"
-        + "Oakland,"
-        + "48326,"
-        + "2200 North Squirrel Road,"
-        + total
-        + ","
-        + sick
-        + ","
-        + feverish
-        + ","
-        +
-        /*sore_throat*/ ","
-        +
-        /*chills*/ ","
-        +
-        /*headache*/ ","
-        +
-        /*muscle_aches*/ ","
-        +
-        /*abdominal_aches*/ ","
-        +
-        /*runny_nose*/ ","
-        +
-        /*nausea_vomiting*/ ","
-        +
-        /*shortness_breath*/ ","
-        +
-        /*loss_taste_smell*/ ","
-        + coughing
-        + ","
-        +
-        /*temp*/ ","
-        + exposed
-        + ","
-    /*workplace_exclusion_travel*/ ;
-  }
-
   public static RowMapper<AnalyticInfo> mapper =
       (rs, rowNum) -> {
         AnalyticInfo info = new AnalyticInfo();
