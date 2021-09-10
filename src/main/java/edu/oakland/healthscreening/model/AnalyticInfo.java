f1f3818cad66a4c534f7dfe305978f79245d179c
Aaron Goodfellow <amgoodfellow@oakland.edu>
2021-09-10 14:34:23 -0400

    refactor!: Updates AnalyticInfo model #117
    BREAKING CHANGE: the AnalyticInfo model being returned from several
    endpoints only contains specific symptoms: 'symptomatic' and 'exposed'

--- src/main/java/edu/oakland/healthscreening/model/AnalyticInfo.java
+++ src/main/java/edu/oakland/healthscreening/model/AnalyticInfo.java
@@ -5,44 +5,32 @@ import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.Data;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
 import org.springframework.jdbc.core.RowMapper;
 
-@Data
+@Builder(toBuilder = true)
+@ToString
+@Getter
 @JsonInclude(NON_NULL)
 public class AnalyticInfo {
   private int total;
-  private int sick;
-  private int congested;
-  private int coughing;
-  private int diarrhea;
+  private int potentiallyPositive;
+  private int symptomatic;
   private int exposed;
-  private int feverish;
-  private int headache;
-  private int lossOfTasteOrSmell;
-  private int muscleAche;
-  private int nauseous;
-  private int shortOfBreath;
-  private int soreThroat;
   private AccountType accountType;
   private Map<AccountType, AnalyticInfo> subTypeAnalytics;
 
   public static RowMapper<AnalyticInfo> mapper =
-      (rs, rowNum) -> {
-        AnalyticInfo info = new AnalyticInfo();
-
-        info.setTotal(rs.getInt("total"));
-        info.setSick(rs.getInt("sick"));
-        info.setCoughing(rs.getInt("coughing"));
-        info.setFeverish(rs.getInt("feverish"));
-        info.setExposed(rs.getInt("exposed"));
-
-        String accountType = rs.getString("account_type");
-
-        if (accountType != null) {
-          info.setAccountType(AccountType.fromString(accountType));
-        }
-
-        return info;
-      };
+      (rs, rowNum) ->
+          AnalyticInfo.builder()
+              .total(rs.getInt("total"))
+              .potentiallyPositive(rs.getInt("potentially_positive"))
+              .exposed(rs.getInt("exposed"))
+              .accountType(
+                  rs.getString("account_type") == null
+                      ? null
+                      : AccountType.fromString(rs.getString("account_type")))
+              .build();
 }
